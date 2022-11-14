8d5f4c195436915aa3f40736325013a9651676f9
Shaimall <sshaimall1738@gmail.com>
2022-11-14 01:35:25 -0800

    3rd commit

--- pages/index.js
+++ pages/index.js
@@ -6,84 +6,52 @@ export default function Home() {
   return (
     <div>
       {/* <Nav /> */}
-      <header className="bg-gradient-to-r to-[#DA4DF1] from-white relative h-[1800px]">
-        <div className="grid grid-cols-12 px-20 gap-2  py-20">
-          <div className="col-span-6 mt-24">
-            <h className="text-5xl leading-10 font-bold">
-              Hello, I’m<br></br> Brooklyn Gilbert
-            </h>
-            <p className="text-base mt-8">
-              I'm a Freelance UI/UX Designer and Developer based in London,
-              England. I strives to build immersive and beautiful web
-              applications through carefully crafted code and user-centric
-              design.
-            </p>
-            <button className="bg-[#A53DFF]  mt-6 rounded-lg px-4 py-1.5 text-xs text-white ">
-              {" "}
-              Say Hello!
-            </button>
-            <div className="grid grid-cols-3 gap-1 text-center py-4 lg:mt-56">
-              <div className="bg-[#EDD8FF] py-4 rounded-lg ">
-                <h className="text-lg font-bold">15 Y.</h>
-                <p className="text-xs"> Experience</p>
-              </div>
-              <div className="bg-[#EDD8FF] py-4 rounded-lg  ">
-                <h className="text-lg font-bold">250+</h>
-                <p className="text-xs "> Projects Completed</p>
-              </div>
-              <div className="bg-[#EDD8FF] py-4 rounded-lg">
-                <h className="text-lg font-bold">58</h>
-                <p className="text-xs "> Happy Client</p>
+      <header className="bg-gradient-to-r to-[#DA4DF1] from-white relative ">
+        <section>
+          <div className=" grid md:grid-cols-2 lg:px-20 md:px-10 gap-2  py-20">
+            <div className="">
+              <h className="lg:text-5xl md:text-3xl leading-normal font-bold text-5xl ">
+                Hello, I’m<br></br> Brooklyn Gilbert
+              </h>
+              <p className="md:text-base mt-8 container xl:pr-36 text-2xl">
+                I'm a Freelance UI/UX Designer and Developer based in London,
+                England. I strives to build immersive and beautiful web
+                applications through carefully crafted code and user-centric
+                design.
+              </p>
+              <button className="bg-[#A53DFF]  mt-6 rounded-md px-4 py-2 text-xs text-white ">
+                {" "}
+                Say Hello!
+              </button>
+              <div className="grid grid-cols-3 gap-1 text-center py-4 lg:mt-48 md:mt-14 xl:mt-64">
+                <div className="bg-[#EDD8FF] py-4 rounded-md ">
+                  <h className="text-lg font-bold">15 Y.</h>
+                  <p className="text-xs"> Experience</p>
+                </div>
+                <div className="bg-[#EDD8FF] py-4 rounded-md  ">
+                  <h className="text-lg font-bold">250+</h>
+                  <p className="text-xs "> Projects Completed</p>
+                </div>
+                <div className="bg-[#EDD8FF] py-4 rounded-md">
+                  <h className="text-lg font-bold">58</h>
+                  <p className="text-xs "> Happy Client</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-span-6 bg bg-no-repeat bg-center bg-[url('/img/man.png')]"></div>
-        </div>
-
-        <div className=" grid grid-cols-2 gap-10 container absolute -bottom-40 left-12 bg-white mt-16  ">
-          <div>
-            <img className="" src="img/man.png" alt="" />
-            {/* <img className="" src="img/card.png" alt="" /> */}
-          </div>
-          <div className="mt-60 ">
-            <h className="text-4xl font-bold">
-              I am Professional User <br></br> Experience Designer
-            </h>
-            <p className="mt-5">
-              I design and develop services for customers specializing creating
-              stylish, modern websites, web services and online stores. My
-              passion is to design digital user experiences.
-            </p>
-            <p className="mt-5">
-              I design and develop services for customers specializing creating
-              stylish, modern websites, web services.
-            </p>
-            <div className="flex gap-5 mt-5">
-              <button className="px-4 py-2 bg-[#A53DFF] text-white">
-                My Project
-              </button>
-              <button className="flex px-4 py-2 border-2 border-[#A53DFF] text-[#A53DFF]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                  />
-                </svg>
-                Download CV
-              </button>
+            <div className=" flex justify-center">
+              <img className="rounded-2xl  " src="img/man1.jpeg" alt="" />
             </div>
           </div>
-        </div>
+        </section>
+        <section>
+          <div className="bg-white mx-auto w-12 ">
+            <div className="grid grid-cols-2">
+              <div></div>
+            </div>
+          </div>
+        </section>
       </header>
-      <section className=" bg-slate-400 ">
+      {/* <section className=" bg-slate-400 ">
         <div className="grid grid-cols-2 container mx-auto pt-40 pb-24">
           <div className=" container px-36 py-16 mt-60">
             <h className=" text-2xl"> Work Process</h>
@@ -104,8 +72,8 @@ export default function Home() {
             ))}
           </div>
         </div>
-      </section>
-      <section>
+      </section> */}
+      {/* <section>
         <h className="w-screen flex justify-center mt-8 text-4xl">Portfolio</h>
         <p className="w-screen flex justify-center mt-7">
           There are many variations of passages of Lorem Ipsum available, but
@@ -116,7 +84,7 @@ export default function Home() {
             <Card2 {...item} key={i} />
           ))}
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
@@ -224,186 +192,186 @@ const CARD = [
   },
 ];
 
-const CARD2 = [
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+// const CARD2 = [
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
-    title2: "Product Admin Dashboard",
+//     title: "UI-UX DESIGN",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
-    title2: "Product Admin Dashboard",
+//     title: "UI-UX DESIGN",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
-    title2: "Product Admin Dashboard",
+//     title: "UI-UX DESIGN",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
-    title2: "Product Admin Dashboard",
+//     title: "UI-UX DESIGN",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
+//     title: "UI-UX DESIGN",
 
-    title2: "Product Admin Dashboard",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-  {
-    image: <img className="" src="img/card1.png" alt="" />,
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+//   {
+//     image: <img className="" src="img/card1.png" alt="" />,
 
-    title: "UI-UX DESIGN",
-    title2: "Product Admin Dashboard",
+//     title: "UI-UX DESIGN",
+//     title2: "Product Admin Dashboard",
 
-    description:
-      "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
-    button: (
-      <button className="flex">
-        {" "}
-        Case Study
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-          />
-        </svg>
-      </button>
-    ),
-    href: "#",
-  },
-];
+//     description:
+//       "Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.",
+//     button: (
+//       <button className="flex">
+//         {" "}
+//         Case Study
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 24 24"
+//           stroke-width="1.5"
+//           stroke="currentColor"
+//           class="w-6 h-6"
+//         >
+//           <path
+//             stroke-linecap="round"
+//             stroke-linejoin="round"
+//             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+//           />
+//         </svg>
+//       </button>
+//     ),
+//     href: "#",
+//   },
+// ];
